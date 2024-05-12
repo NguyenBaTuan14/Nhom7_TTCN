50b1176f0c66eecd17c03d5b21ccc5bb7dc9e00b
Thu Thuy <nguyenxuanduc504@gmail.com>
2024-05-12 20:48:11 +0700

    init project

---
+++ TTCN/HoKhau.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TTCN
+{
+    /// <summary>
+    /// Interaction logic for HoKhau.xaml
+    /// </summary>
+    public partial class HoKhau : Window
+    {
+        public HoKhau()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 wd = new Window1();
+            wd.Show();
+            Close();
+        }
+    }
+}
