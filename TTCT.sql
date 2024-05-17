5332456946a25be02aad49ca4c32c987490350df
Nguyen Ba Tuan <nbt14082003thptnt@gmail.com>
2024-05-17 13:39:35 +0700

    Them file

---
+++ TTCT.sql
@@ -0,0 +1,126 @@
+/*
+Created		4/27/2024
+Modified		5/12/2024
+Project		
+Model		
+Company		
+Author		
+Version		
+Database		MS SQL 7 
+*/
+use master
+go
+Create database QLThongTinDanCu
+go
+use QLThongTinDanCu
+go
+/*Drop table [ThongBaoXaGui] 
+go
+Drop table [HoiDap] 
+go
+Drop table [DiaChi] 
+go
+Drop table [ChuHo] 
+go
+Drop table [HoKhau] 
+go
+Drop table [NhanKhau] 
+go
+Drop table [TaiKhoan] 
+go
+*/
+
+Create table [TaiKhoan] (
+	[MaTaiKhoan] nvarChar(50) NOT NULL,
+	[Ho] nvarChar(35) NULL,
+	[Ten] nvarChar(15) NULL,
+	[SoDienThoai] nvarChar(10) NULL,
+	[Email] nvarChar(225) NULL,
+	[AnhChanDung] image NULL,
+	[MatKhau] nvarChar(50) NULL,
+Primary Key  ([MaTaiKhoan])
+) 
+
+Create table [NhanKhau] (
+	[MaNhanKhau] nvarChar(50) NOT NULL,
+	[HoTen] nvarChar(50) NULL,
+	[QuanHeVoiChuHo] nvarChar(60) NULL,
+	[SoCMND_CCCD] nvarChar(50) NULL,
+	[GioiTinh] bit NULL,
+	[NgaySinh] date NULL,
+	[TrinhDoHocVan] nvarChar(50) NULL,
+	[NgheNghiep] nvarChar(50) NULL,
+	[TonGiao] nvarChar(50) NULL,
+	[DiaChiThuongChu] nvarChar(100) NULL,
+	[TinhTrangHonNhan] nvarChar(50) NULL,
+	[DanToc] nvarChar(50) NULL,
+	
+	[MaHoKhau] nvarChar(50) NOT NULL,
+Primary Key  ([MaNhanKhau])
+) 
+go
+
+Create table [HoKhau] (
+	[MaHoKhau] nvarChar(50) NOT NULL,
+	[NgayDangKy] date NULL,
+	[NgayChuyenDi] date NULL,
+	[MaTaiKhoan] nvarChar(50) NOT NULL,
+	[MaChuHo] nvarChar(50) NOT NULL,
+	[MaVung] nvarChar(50) NOT NULL,
+Primary Key  ([MaHoKhau])
+) 
+go
+
+Create table [ChuHo] (
+	[MaChuHo] nvarChar(50) NOT NULL,
+	[TenChuHo] nvarChar(50) NULL,
+	[NgaySinh] date NULL,
+	[GioiTinh] bit NULL,
+	[QueQuan] nvarChar(50) NULL,
+	[DanToc] nvarChar(50) NULL,
+	[TonGiao] nvarChar(50) NULL,
+	[SoCMND_CCCD] nvarChar(50) NULL,
+	[NgheNghiep] nvarChar(50) NULL,
+	[NoiLamViec] nvarChar(50) NULL,
+	[NgayChuyenDen] date NULL,
+	[NoiThuongChu] nvarChar(50) NULL,
+	[TinhTrangHonNhan] nvarChar(50) NULL,
+Primary Key  ([MaChuHo])
+) 
+go
+
+Create table [DiaChi] (
+	[MaVung] nvarChar(50) NOT NULL,
+	[TenTinh_ThanhPho] nvarChar(50) NULL,
+	[TenQuan_Huyen] nvarChar(50) NULL,
+	[TenPhuong_Xa] nvarChar(50) NULL,
+	[SoNha] nvarChar(10) NULL,
+	[TenThon_Xom] nvarChar(50) NULL,
+Primary Key  ([MaVung])
+) 
+go
+
+Create table [HoiDap] (
+	[MaCauHoi] nvarChar(50) NOT NULL,
+	[NoiDungCauHoi] text NULL,
+	[NoiDungCauTraLoi] text NULL,
+	[MaTaiKhoan] nvarChar(50) NOT NULL,
+Primary Key  ([MaCauHoi])
+) 
+go
+
+Create table [ThongBaoXaGui] (
+	[MaThongBao] nvarChar(50) NOT NULL,
+	[NoiDungThongBao] text NULL,
+	[NgayThongBao] date NOT NULL,
+	[MaTaiKhoan] nvarChar(50) NOT NULL,
+Primary Key  ([MaThongBao])
+) 
+go
+--Set quoted_identifier on
+--go
+--Set quoted_identifier off
+--go
+
+
+
