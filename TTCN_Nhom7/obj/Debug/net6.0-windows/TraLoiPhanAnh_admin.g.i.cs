f838b66eafb5ac75729a8a1350f8dd728164d17a
Nguyen Ba Tuan <nbt14082003thptnt@gmail.com>
2024-06-02 09:27:28 +0700

    Nguyen Ba Tuan

--- TTCN_Nhom7/obj/Debug/net6.0-windows/TraLoiPhanAnh_admin.g.i.cs
+++ TTCN_Nhom7/obj/Debug/net6.0-windows/TraLoiPhanAnh_admin.g.i.cs
@@ -69,7 +69,7 @@ namespace TTCN_Nhom7 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TTCN_Nhom7;component/traloiphananh_admin.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TTCN_Nhom7;V1.0.0.0;component/traloiphananh_admin.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\TraLoiPhanAnh_admin.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
